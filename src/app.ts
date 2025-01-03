ccbb563fd60262aa8c1b5d0eae2a3ad238d97cf6
me 8220 <mel@dev.vto.group>
2025-01-03 15:39:12 +0530

    feat: update CORS settings to allow requests from production domain

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://stocks-academy.vercel.app/']
 }));
+
 
 // Middleware
 
